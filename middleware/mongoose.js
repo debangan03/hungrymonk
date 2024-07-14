a97940030fa94cc55381b8933f54a70d09ab02e5
Debangan Bhattacharyya <112092614+debangan03@users.noreply.github.com>
2024-07-15 01:56:03 +0530

    jai maaa

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const conndb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   } else {
-    mongoose.connect(`mongodb+srv://nandibibaswan19:${secrets.MONGO_PASSWORD}@cluster0.rlr4xvv.mongodb.net/trial?retryWrites=true&w=majority&appName=Cluster0`);
+    mongoose.connect(`mongodb+srv://nandibibaswan19:klkbN79uuV8mKfrn@cluster0.rlr4xvv.mongodb.net/trial?retryWrites=true&w=majority&appName=Cluster0`);
     return handler(req, res);
   }
 };
